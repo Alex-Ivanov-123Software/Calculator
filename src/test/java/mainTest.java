0950d62600b8fb21912c726c1409a171a63f87ed
Alex-Ivanov-123Software <alexanderiv@123software.ru>
2016-04-27 10:54:41 +0300

    qqcqwcqwc

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -18,6 +18,6 @@ public class mainTest extends main {
 
     @Test
     public void string() throws Exception {
-        assertEquals(string("Total"),"Total 1000");
+        assertEquals(string("Total"),"Total 2000");
     }
 }
